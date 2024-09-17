e750dfc2bd94e7ef7d47f6df6b5803d9c41b3fc5
vodohleb <yackub563@mail.ru>
2024-09-17 20:25:10 +0300

    replace custom model class with function, that returns the basic model class (is needed to make model serializable)

--- backend/agents/recommendations_agent/critic_model.py
+++ backend/agents/recommendations_agent/critic_model.py
@@ -9,15 +9,21 @@ import tensorflow
 HE_INIT = keras.initializers.HeNormal()
 
 
-class CriticModel(keras.Model):
+def get_critic_model(
+    dtype: tf.DType,
+    state_size: int, action_size: int,
+    state_input_units: int, action_input_units,
+    state_hidden_units: List[int] = None,
+    action_hidden_units: List[int] = None,
+    concat_hidden_units: List[int] = None
+):
     """
     Model to generate Q-value for the given state and action.
     Model is based on DDPG Critic model from https://arxiv.org/pdf/1509.02971.
 
     Check https://drive.google.com/file/d/1GQIfN6j1q9uASHh9LYubgxs5XPQtwQyy/view?usp=sharing to understand structure of Critic model.
     """
-    @staticmethod
-    def _define_hidden_network(hidden_units: List[int], dtype: tf.DType):
+    def define_hidden_network(hidden_units: List[int], dtype: tf.DType):
         hidden_layers = [
             None for _ in range(3 * len(hidden_units))
         ]
@@ -32,67 +38,33 @@ class CriticModel(keras.Model):
             i += 1
         
         return keras.Sequential(hidden_layers)
-        
 
-    def __init__(
-            self,
-            dtype: tf.DType,
-            state_input_units: int, action_input_units,
-            state_hidden_units: List[int] | None = None,
-            action_hidden_units: List[int] | None = None,
-            concat_hidden_units: List[int] | None = None
-        ):
-        super().__init__()
-
-        self._state_hidden_network = None
-        self._action_hidden_network = None
-        self._concat_hidden_network = None
-
-        self._state_input_dense = layers.Dense(state_input_units, use_bias=False, kernel_initializer=HE_INIT, dtype=dtype)
-        self._state_input_batch_norm = layers.BatchNormalization(dtype=dtype)
-
-        if state_hidden_units:
-            self._state_hidden_network = self._define_hidden_network(state_hidden_units, dtype)
-        
-        self._action_input_dense = layers.Dense(action_input_units, use_bias=False, kernel_initializer=HE_INIT, dtype=dtype)
-        self._action_input_batch_norm = layers.BatchNormalization(dtype=dtype)
-        
-        if action_hidden_units:
-            self._action_hidden_network = self._define_hidden_network(action_hidden_units, dtype)
-
-        if concat_hidden_units:
-            self._concat_hidden_network = self._define_hidden_network(concat_hidden_units, dtype)
-
-        self._concat_output_dense = layers.Dense(1, use_bias=True, dtype=dtype)
-
+    state_input = layers.Input((state_size,), dtype=dtype)
+    state = layers.Dense(state_input_units, use_bias=False, kernel_initializer=HE_INIT, dtype=dtype)(state_input)
+    state = layers.BatchNormalization(dtype=dtype)(state)
+    state = layers.LeakyReLU(negative_slope=0.3)(state)
+    if state_hidden_units:
+        state = define_hidden_network(state_hidden_units, dtype)(state)
     
-    def __call__(self, state, action):
-        state = self._state_input_dense(state)
-        state = self._state_input_batch_norm(state)
-        state = tf.nn.leaky_relu(state, alpha=0.3)
+    action_input = layers.Input((action_size,), dtype=dtype)
+    action = layers.Dense(action_input_units, use_bias=False, kernel_initializer=HE_INIT, dtype=dtype)(action_input)
+    action = layers.BatchNormalization(dtype=dtype)(action)
+    action = layers.LeakyReLU(negative_slope=0.3)(action)
+    if action_hidden_units:
+        action = define_hidden_network(action_hidden_units, dtype)(action)
+    
+    concat = layers.Concatenate()([state, action])
+    if concat_hidden_units:
+        concat = define_hidden_network(concat_hidden_units, dtype)(concat)
 
-        if self._state_hidden_network:
-            state = self._state_hidden_network(state)
+    critic_value = layers.Dense(1, use_bias=True, dtype=dtype)(concat)
 
-        action = self._action_input_dense(action)
-        action = self._action_input_batch_norm(action)
-        action = tf.nn.leaky_relu(action, alpha=0.3)
-
-        if self._action_hidden_network:
-            action = self._action_hidden_network(action)
-
-        critic_value = tf.concat([state, action], axis=1)
-
-        if self._concat_hidden_network:
-            critic_value = self._concat_hidden_network(critic_value)
-        
-        critic_value = self._concat_output_dense(critic_value)
-
-        return critic_value
+    return keras.Model([state_input, action_input], critic_value)
+    
 
 
 if __name__ == "__main__":
-    critic = CriticModel(tf.float32, 16, 16, [32, 64], [32, 64], [256, 256])
+    critic = get_critic_model(tf.float32, 3, 1, 16, 16, [32, 64], [32, 64], [256, 256])
     
     action = tf.convert_to_tensor([[0.53]], dtype=tf.float32)
     state = tf.convert_to_tensor([[1., 1., 1.]], dtype=tf.float32)
@@ -102,7 +74,7 @@ if __name__ == "__main__":
     right = tf.convert_to_tensor(1, dtype=tf.float32)
 
     with tf.GradientTape() as tape:
-        critic_value = critic(state, action)
+        critic_value = critic([state, action])
 
         loss = tf.reduce_mean(tf.square(right - critic_value))
         print(loss)
