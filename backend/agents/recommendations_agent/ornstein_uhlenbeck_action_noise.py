423c4a29b0bcd34d116fbd3b52d10ae43e6cb28e
vodohleb <yackub563@mail.ru>
2024-09-23 16:49:13 +0300

    add reinit of random process

--- backend/agents/recommendations_agent/ornstein_uhlenbeck_action_noise.py
+++ backend/agents/recommendations_agent/ornstein_uhlenbeck_action_noise.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 class OrnsteinUhlenbeckNoise:
-    def __init__(self, dtype, mean, std_deviation, theta=0.15, dt=1e-2, x_initial=None):
+    def __init__(self, dtype, mean, std_deviation, theta=0.15, dt=1e-2, x_initial=None, reinit_x_prev_period=1000):
         super().__init__()
         self.dtype = dtype
         self.theta = theta
@@ -11,9 +11,14 @@ class OrnsteinUhlenbeckNoise:
         self.std_dev = std_deviation
         self.dt = dt
         if x_initial is not None:
-            self.x_prev = x_initial
+            self._x_initial = x_initial
         else:
-            self.x_prev = np.zeros_like(self.mean, dtype=dtype)
+            self._x_initial = np.zeros_like(self.mean, dtype=dtype)
+        
+        self.x_prev = self._x_initial
+
+        self._reinit_x_prev_period = reinit_x_prev_period
+        self._after_last_reinit = 0
 
 
     def __call__(self):
@@ -23,5 +28,10 @@ class OrnsteinUhlenbeckNoise:
             self.std_dev * np.sqrt(self.dt) * 
             np.random.normal(size=self.mean.shape, loc=0.0, scale=1.0).astype(self.dtype)
         )
-        self.x_prev = x
+        self._after_last_reinit += 1
+        if self._after_last_reinit == self._reinit_x_prev_period:
+            self.x_prev = self._x_initial
+            self._after_last_reinit = 0
+        else:
+            self.x_prev = x
         return x
