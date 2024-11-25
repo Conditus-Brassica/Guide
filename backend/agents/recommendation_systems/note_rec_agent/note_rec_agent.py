d8c2c6224a4dfae849baef4bd77e9aba0820c1ec
yackub <yackub563@mail.ru>
2024-11-25 17:12:10 +0300

    finish note rec agent implementation (tests are needed)

--- backend/agents/recommendation_systems/note_rec_agent/note_rec_agent.py
+++ backend/agents/recommendation_systems/note_rec_agent/note_rec_agent.py
@@ -12,15 +12,9 @@ from backend.agents.recommendation_systems.note_rec_sys_user_reaction import Use
 from backend.broker.abstract_agents_broker import AbstractAgentsBroker
 from backend.broker.agents_tasks.note_embeddings_crud_agent_tasks import get_notes_by_titles, get_nearest_notes
 
-
-# TODO change with my trainer
-from backend.broker.agents_tasks.landmark_trainer_tasks import (
-    partial_record_with_next_state,
-    fill_up_partial_record_reward_only_list,
-    fill_up_partial_record_reward_only_replace_next_state_list,
-    record_list,
-    train, get_state,
-    get_actor_model, get_critic_model, get_actor_model_config, get_critic_model_config
+from backend.broker.agents_tasks.note_trainer_tasks import (
+    partial_record_with_next_state, fill_up_partial_record_reward_only_list,
+    train, get_actor_model, get_critic_model, get_actor_model_config, get_critic_model_config
 )
 
 
@@ -169,7 +163,7 @@ class NoteRecAgent(PureNoteRecAgent):
         embeddings_async_task.add_done_callback(self._asyncio_tasks.discard)
 
         embeddings_asyncio_result = await embeddings_async_task
-        logger.debug(
+        await logger.debug(
             f"Recommendations agent, _embeddings_for_notes, "
             f"embeddings_for_landmarks: {embeddings_asyncio_result}"
         )
@@ -212,7 +206,7 @@ class NoteRecAgent(PureNoteRecAgent):
 
 
     @staticmethod
-    def _interaction_with_note_finished(json_params):
+    def _interactions_with_note_finished(json_params):
         """This method checks values only of special params. Other values will be checked in target agent."""
         validate(json_params, json_validation.interaction_with_note_finished)
         if json_params["relative_interaction_time"] < 0 or json_params["relative_interaction_time"] > 1:
@@ -246,7 +240,7 @@ class NoteRecAgent(PureNoteRecAgent):
         embeddings_async_task.add_done_callback(self._asyncio_tasks.discard)
 
         embeddings_asyncio_result = await embeddings_async_task
-        logger.debug(
+        await logger.debug(
             f"Recommendations agent, _get_nearest_notes, "
             f"embeddings_for_landmarks: {embeddings_asyncio_result}"
         )
@@ -288,7 +282,6 @@ class NoteRecAgent(PureNoteRecAgent):
         return recommendations
 
 
-    # TODO change with note trainer
     @staticmethod
     async def _partial_record_with_next_state_task(state, action, next_state):
         partial_record_list_async_task = asyncio.create_task(
@@ -383,7 +376,7 @@ class NoteRecAgent(PureNoteRecAgent):
             return {}  # raise ValidationError
 
         if not self._requires_training:
-            logger.info("RecSys agent doesn't require training. No sars records will be completed")
+            await logger.info("RecSys agent doesn't require training. No sars records will be completed")
             return {}
 
         state = np.asarray(json_params["state"], dtype=self._np_dtype)
@@ -392,34 +385,11 @@ class NoteRecAgent(PureNoteRecAgent):
         note_embedding = np.asarray(note_embedding[json_params["note_title"]], dtype=self._np_dtype)
 
         next_state = self._new_state_formula(state, note_embedding)
-        index, uuid = await self._partial_record_with_next_state_task(state, note_embedding, next_state)
+        index, uuid = await self._partial_record_with_next_state_task(
+            state.tolist(), note_embedding.tolist(), next_state.tolist()
+        )
 
         return {"row_uuid": uuid, "row_index": index}
-
-
-
-    # TODO
-    def _give_reward_to_recommendations(
-        self, primary_recommendations, result_recommendations, user_reward
-    ):
-        """
-        Remove from result_recommendations landmarks that included in primary_recommendations
-        (primary_recommendations and result_recommendations will be modified). Reward for result_recommendations,
-        that were included by user wil be given in _post_result_recs_to_sars_buffer method.
-        """
-        reward = self.reward_function(user_reward)
-
-        for primary_recommendation in primary_recommendations:
-            for index_in_result in range(len(result_recommendations)):
-                if self._landmarks_are_equal(primary_recommendation, result_recommendations[index_in_result]):
-                    primary_recommendation["reward"] = reward
-                    primary_recommendation["change_next_state"] = False
-                    result_recommendations.pop(index_in_result)
-                    break
-            else:
-                primary_recommendation["reward"] = self._min_reward  # Min reward for landmarks, removed from route by user
-                primary_recommendation["change_next_state"] = True
-        return primary_recommendations, result_recommendations
     
 
     @staticmethod
@@ -436,135 +406,8 @@ class NoteRecAgent(PureNoteRecAgent):
 
 
     @staticmethod
-    async def _fill_up_partial_record_reward_only_replace_next_state_list_task(row_index_list, row_uuid_list, reward_list):
-        fill_up_partial_record_reward_only_replace_next_state_list_task = asyncio.create_task(
-            AbstractAgentsBroker.call_agent_task(
-                fill_up_partial_record_reward_only_replace_next_state_list,
-                json_params={
-                    "row_index_list": row_index_list, "row_uuid_list": row_uuid_list, "reward_list": reward_list
-                }
-            )
-        )
-        return (await fill_up_partial_record_reward_only_replace_next_state_list_task).return_value
-
-
-    @staticmethod
-    async def _get_state_task(row_index, row_uuid):
-        get_state_async_task = asyncio.create_task(
-            AbstractAgentsBroker.call_agent_task(
-                get_state, {"row_index": row_index, "row_uuid": row_uuid}
-            )
-        )
-        return (await get_state_async_task).return_value
-    
-
-    @staticmethod
-    async def _record_list_task(sars_tuple_list):
-        record_list_async_task = asyncio.create_task(
-            AbstractAgentsBroker.call_agent_task(
-                record_list, {"sars_tuple_list": sars_tuple_list}
-            )
-        )
-        return (await record_list_async_task).return_value
-
-
-    async def _define_state_using_primary_uuids(
-        self, primary_recommendations, uuid_correct_list, uuid_correct_list_with_state_replace
-    ):
-        state = None
-        for i in range(len(uuid_correct_list)):
-            if uuid_correct_list[i]:
-                state = await self._get_state_task(
-                    primary_recommendations[i]["row_index"], primary_recommendations[i]["row_uuid"]
-                )
-                if state is not None:
-                    return state
-
-        for i in range(len(uuid_correct_list_with_state_replace)):
-            if uuid_correct_list_with_state_replace[i]:
-                state = await self._get_state_task(
-                    primary_recommendations[i]["row_index"], primary_recommendations[i]["row_uuid"]
-                )
-                if state is not None:
-                    return state
-
-        return state
-
-
-    @staticmethod
-    def _post_primary_recs_to_sars_buffer_debug_messages(uuid_correct_list, uuid_correct_list_with_state_replace):
-        logger.debug(
-            f"_post_primary_recs_to_sars_buffer; "
-            f"filled_up {uuid_correct_list.count(True)}/{len(uuid_correct_list)} of primary recommendations "
-            f"without replace of next_state"
-        )
-        logger.debug(
-            f"_post_primary_recs_to_sars_buffer; "
-            f"filled_up {uuid_correct_list_with_state_replace.count(True)}/{len(uuid_correct_list_with_state_replace)} "
-            f"of primary recommendations with replace of next_state"
-        )
-
-
-    async def _post_primary_recs_to_sars_buffer(self, primary_recommendations):
-        """
-        returns: state
-            state: numpy.ndarray | None (array, if at least one partial record was filled up, state(n) of this record)
-        """
-        row_index_list = []
-        row_uuid_list = []
-        reward_list = []
-
-        change_next_state_row_index_list = []
-        change_next_state_row_uuid_list = []
-        change_next_state_reward_list = []
-        for i in range(len(primary_recommendations)):
-            if primary_recommendations[i]["change_next_state"]:
-                change_next_state_row_index_list.append(primary_recommendations[i]["row_index"])
-                change_next_state_row_uuid_list.append(primary_recommendations[i]["row_uuid"])
-                change_next_state_reward_list.append(primary_recommendations[i]["reward"])
-            else:
-                row_index_list.append(primary_recommendations[i]["row_index"])
-                row_uuid_list.append(primary_recommendations[i]["row_uuid"])
-                reward_list.append(primary_recommendations[i]["reward"])
-
-        uuid_correct_list = await self._fill_up_partial_record_reward_only_list_task(
-            row_index_list, row_uuid_list, reward_list
-        )
-        uuid_correct_list_with_state_replace = await self._fill_up_partial_record_reward_only_replace_next_state_list_task(
-            change_next_state_row_index_list, change_next_state_row_uuid_list, change_next_state_reward_list
-        )
-
-        self._post_primary_recs_to_sars_buffer_debug_messages(uuid_correct_list, uuid_correct_list_with_state_replace)
-
-        return await self._define_state_using_primary_uuids(
-            primary_recommendations, uuid_correct_list, uuid_correct_list_with_state_replace
-        )
-
-
-    async def _post_result_recs_to_sars_buffer(self, result_recommendations, state: None | List[float]):
-        if result_recommendations and state is not None:
-            actions = await self._embeddings_for_notes(result_recommendations)
-
-            reward = self._max_reward
-
-            watch_state, visit_state = self._split_state(np.asarray(state, dtype=self._np_dtype))
-
-            next_state_list = self._count_next_states_for_actions(
-                watch_state, visit_state, actions, count_using_tensorflow=False
-            ).tolist()
-
-            sars_tuple_list = []
-            for i in range(len(result_recommendations)):
-                sars_tuple_list.append((state, actions[i], reward, next_state_list[i]))
-
-            await self._record_list_task(sars_tuple_list)
-
-            logger.debug(f"_post_result_recs_to_sars_buffer; {len(result_recommendations)} records added from landmarks, given by user")
-    
-
-    @staticmethod
     async def _start_training(repeat_amount):
-        logger.debug(f"{repeat_amount} training cycles")
+        await logger.debug(f"{repeat_amount} training cycles")
         train_async_task = asyncio.create_task(
             AbstractAgentsBroker.call_agent_task(
                 train, {"repeat_amount": repeat_amount}
@@ -573,30 +416,46 @@ class NoteRecAgent(PureNoteRecAgent):
         return (await train_async_task).return_value
 
 
-    async def post_result_of_recommendations(self, json_params):
-        logger.debug(f"post_result_of_recommendations")
-        if not self._requires_training:
-            logger.info("RecSys agent doesn't requires training.")
-            return
-
-        try:
-            self._post_result_of_recommendations_validation(json_params)
-        except ValidationError as ex:
-            await logger.error(f"post_result_of_recommendations, ValidationError({ex.args[0]})")
-            return  # raise ValidationError
-
-        primary_recommendations, result_recommendations = self._give_reward_to_recommendations(
-            json_params["primary_recommendations"],
-            json_params["result_recommendations"],
-            json_params["user_reward"]
+    @staticmethod
+    async def _interactions_with_notes_finished_debug_messages(uuid_correct_list):
+        await logger.debug(
+            f"_interactions_with_notes_finished; "
+            f"filled_up {uuid_correct_list.count(True)}/{len(uuid_correct_list)} of finished interactions"
         )
-        
-        state = await self._post_primary_recs_to_sars_buffer(primary_recommendations)
-        await self._post_result_recs_to_sars_buffer(result_recommendations, state)
-        
-        new_models_json = await self._start_training(
-            len(primary_recommendations) + len(result_recommendations)
+
+
+    async def _interactions_with_notes_finished(self, json_params: Dict):
+        row_uuid_list = []
+        row_index_list = []
+        reward_list = []
+        for interaction in json_params["interactions"]:
+            row_uuid_list.append(interaction["row_uuid"])
+            row_index_list.append(interaction["row_index"])
+            reward_list.append(
+                self.reward_function(interaction["relative_interaction_time"], interaction["user_reaction"])
+            )
+
+        uuid_correct_list = await self._fill_up_partial_record_reward_only_list_task(
+            row_index_list, row_uuid_list, reward_list
         )
+        await self._interactions_with_notes_finished_debug_messages(uuid_correct_list)
+
+        new_models_json = await self._start_training(len(json_params["interactions"]))
+
         self._update_model(self._actor_model, new_models_json["actor_model"], self._np_dtype)
         self._update_model(self._critic_model, new_models_json["critic_model"], self._np_dtype)
 
+
+    async def interactions_with_notes_finished(self, json_params):
+        await logger.debug(f"interactions_with_notes_finished")
+        if not self._requires_training:
+            await logger.info("RecSys agent doesn't requires training.")
+            return
+
+        try:
+            validate(json_params, json_validation.interaction_with_note_finished)
+        except ValidationError as ex:
+            await logger.error(f"interactions_with_notes_finished, ValidationError({ex.args[0]})")
+            return  # raise ValidationError
+
+        self._interactions_with_note_finished(json_params)
