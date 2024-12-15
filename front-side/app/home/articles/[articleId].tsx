c19db000215f61fd07b57c7c31b846ee5c2b9704
Ivan Titlov <super.titlov@inbox.ru>
2024-12-15 22:19:11 +0300

    changes

--- front-side/app/home/articles/[articleId].tsx
+++ front-side/app/home/articles/[articleId].tsx
@@ -4,84 +4,109 @@ import { BASE_URL } from "@/constants/request-api-constants";
 import { FirebaseAuth } from "@/FirebaseConfig";
 import axios from "axios";
 import { useLocalSearchParams } from "expo-router";
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
 import Markdown from "react-native-markdown-display";
 import { ArticlesInfo } from "../(tabs)/articles";
 import { ArticleScoreComponent } from "@/components/ArticleScoreComponent";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { articleRewardKey } from "@/constants/async-storage-constants";
 
 type ArticleInfo = {
-	content: string;
+  content: string;
 } & ArticlesInfo;
 
 type ArticleReward = {
-	articleId: string;
-	timeOnPage: number;
-	score: ArticleScore;
+  articleName: string;
+  timeOnPage: number;
+  score: ArticleScore;
 } & UserInfo;
 
+const GetArticleInfoFromStorage = async (id: string) => {
+  const articlesStore = await AsyncStorage.getItem("Articles_State");
+  const articles = JSON.parse(articlesStore ?? "") as Array<ArticleReward>;
+  if (articles.some((article) => article.articleName == id)) {
+    const article = articles.find((article) => article.articleName == id);
+    return article as ArticleReward;
+  }
+  return undefined;
+};
+
 const Article: FC = () => {
-	const [isLoaded, setIsLoaded] = useState(false);
-	const [articleDetails, setIsArticleDetails] = useState<ArticleInfo | null>(
-		null
-	);
-	const [score, setScore] = useState<ArticleScore>(
-		articleDetails?.score ?? ArticleScore.POHUY
-	);
-	const id = String(useLocalSearchParams());
-	const user = String(FirebaseAuth.currentUser?.uid);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [articleDetails, setIsArticleDetails] = useState<ArticleInfo | null>(
+    null
+  );
+  const initialTime = useRef(0);
+  const [score, setScore] = useState<ArticleScore>(
+    articleDetails?.score ?? ArticleScore.POHUY
+  );
+  const id = String(useLocalSearchParams());
+  const user = String(FirebaseAuth.currentUser?.uid);
 
-	const getArticle = async () => {
-		try {
-			const url = new URL(BASE_URL);
-			url.searchParams.set("id", id);
-			const response = await axios.get<ArticleInfo>(url.toString());
-			setIsArticleDetails(response.data);
-			setScore(response.data.score);
-		} catch (error) {
-			console.error(`HTTP error: ${error}`);
-		} finally {
-			setIsLoaded(true);
-		}
-	};
+  const firstInteraction = async (articleDetails: ArticleInfo) => {
+    await axios.post(BASE_URL + "/articles/state/interation_started", [
+      { title: articleDetails.title },
+    ]);
+  };
 
-	const postArticleReward = async (articleReward: ArticleReward) => {
-		try {
-			const url = new URL(BASE_URL);
-			url.searchParams.set("id", String(id));
-			navigator.sendBeacon(url.toString(), JSON.stringify(articleReward));
-		} catch (error) {
-			console.error(`HTTP error: ${error}`);
-		}
-	};
+  const getArticle = async () => {
+    try {
+      const url = BASE_URL + "/articles/" + id;
+      const response = await axios.get<ArticleInfo>(url);
+      setIsArticleDetails(response.data);
+      const articleInfo = await GetArticleInfoFromStorage(id);
+      if (articleInfo) {
+        setScore(articleInfo.score);
+        firstInteraction(response.data);
+        initialTime.current = articleInfo.timeOnPage;
+      } else {
+        setScore(response.data.score);
+      }
+    } catch (error) {
+      console.error(`HTTP error: ${error}`);
+    } finally {
+      setIsLoaded(true);
+    }
+  };
 
-	useEffect(() => {
-		getArticle();
-		const entryTime = Date.now();
-		return () => {
-			const exit = Date.now();
-			const duration = exit - entryTime;
-			postArticleReward({
-				articleId: id,
-				timeOnPage: duration,
-				score: score,
-				userId: user,
-			});
-		};
-	}, []);
+  const postArticleReward = async (articleReward: ArticleReward) => {
+    try {
+      var articlesReward = JSON.stringify(articleReward);
+      await AsyncStorage.mergeItem(articleRewardKey, articlesReward);
+    } catch (error) {
+      console.error(`HTTP error: ${error}`);
+    }
+  };
 
-	return (
-		<ScrollView
-			contentInsetAdjustmentBehavior="automatic"
-			style={{ height: "100%" }}
-		>
-			<Markdown>{articleDetails?.content}</Markdown>
-			<ArticleScoreComponent
-				score={articleDetails?.score ?? ArticleScore.POHUY}
-				setScore={setScore}
-			/>
-		</ScrollView>
-	);
+  useEffect(() => {
+    getArticle();
+    const entryTime = Date.now();
+    return () => {
+      if (!articleDetails) return;
+      const exit = Date.now();
+      const duration = exit - entryTime + initialTime.current;
+      postArticleReward({
+        articleName: articleDetails.title,
+        timeOnPage: duration,
+        score: score,
+        userId: user,
+      });
+    };
+  }, []);
+
+  return (
+    <ScrollView
+      contentInsetAdjustmentBehavior="automatic"
+      style={{ height: "100%" }}
+    >
+      <Markdown>{articleDetails?.content}</Markdown>
+      <ArticleScoreComponent
+        score={articleDetails?.score ?? ArticleScore.POHUY}
+        setScore={setScore}
+      />
+    </ScrollView>
+  );
 };
 
 export default Article;
--- front-side/app/login.tsx
+++ front-side/app/login.tsx
@@ -1,6 +1,7 @@
 import {
-	storageUserEmailKey,
-	storageUserPasswordKey,
+  articleRewardKey,
+  storageUserEmailKey,
+  storageUserPasswordKey,
 } from "@/constants/async-storage-constants";
 import { Colors } from "@/constants/Colors";
 import { BASE_URL } from "@/constants/request-api-constants";
@@ -9,227 +10,234 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import { router } from "expo-router";
 import {
-	createUserWithEmailAndPassword,
-	signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
 } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import {
-	KeyboardAvoidingView,
-	Text,
-	View,
-	StyleSheet,
-	TouchableOpacity,
+  KeyboardAvoidingView,
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
 } from "react-native";
 import { HelperText, TextInput } from "react-native-paper";
 
 export type UserInfo = {
-	userId: string;
+  userId: string;
 };
 
 type ErrorType = {
-	[key: string]: string;
+  [key: string]: string;
 };
 
 const LoginScreen = () => {
-	const [form, setForm] = useState({ email: "", password: "" });
-	const [errors, setErrors] = useState<ErrorType>({});
+  const [form, setForm] = useState({ email: "", password: "" });
+  const [errors, setErrors] = useState<ErrorType>({});
 
-	const postUserInfo = () => {
-		try {
-			const user = FirebaseAuth.currentUser;
-			axios.post<UserInfo>(BASE_URL, { userId: user?.uid });
-		} catch (error) {
-			console.error(`HTTP request error: ${error}`);
-		}
-	};
+  const postUserInfo = () => {
+    try {
+      const user = FirebaseAuth.currentUser;
+      axios.post<UserInfo>(BASE_URL, { userId: user?.uid });
+    } catch (error) {
+      console.error(`HTTP request error: ${error}`);
+    }
+  };
 
-	const handleSignup = async () => {
-		try {
-			await createUserWithEmailAndPassword(
-				FirebaseAuth,
-				form.email,
-				form.password
-			);
-			postUserInfo();
-			await AsyncStorage.setItem(storageUserEmailKey, form.email);
-			await AsyncStorage.setItem(storageUserPasswordKey, form.password);
-			setErrors({});
-		} catch (error: any) {
-			const parsedErrors = parseFirebaseError(error);
-			setErrors(parsedErrors);
-		}
-	};
+  const handleSignup = async () => {
+    try {
+      await createUserWithEmailAndPassword(
+        FirebaseAuth,
+        form.email,
+        form.password
+      );
+      postUserInfo();
+      await AsyncStorage.setItem(storageUserEmailKey, form.email);
+      await AsyncStorage.setItem(storageUserPasswordKey, form.password);
+      setErrors({});
+    } catch (error: any) {
+      const parsedErrors = parseFirebaseError(error);
+      setErrors(parsedErrors);
+    }
+  };
 
-	const handleLogin = async () => {
-		try {
-			await signInWithEmailAndPassword(FirebaseAuth, form.email, form.password);
-			await AsyncStorage.setItem(storageUserEmailKey, form.email);
-			await AsyncStorage.setItem(storageUserPasswordKey, form.password);
-			setErrors({});
-		} catch (error: any) {
-			const parsedErrors = parseFirebaseError(error);
-			setErrors(parsedErrors);
-		}
-	};
+  const handleLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(FirebaseAuth, form.email, form.password);
+      const articlesStore = await AsyncStorage.getItem(articleRewardKey);
+      AsyncStorage.removeItem(articleRewardKey);
+      if (articlesStore != null)
+        navigator.sendBeacon(
+          BASE_URL + "/articles/interaction_finished",
+          articlesStore
+        );
+      await AsyncStorage.setItem(storageUserEmailKey, form.email);
+      await AsyncStorage.setItem(storageUserPasswordKey, form.password);
+      setErrors({});
+    } catch (error: any) {
+      const parsedErrors = parseFirebaseError(error);
+      setErrors(parsedErrors);
+    }
+  };
 
-	const handleFormChange = (key: "email" | "password", value: string) => {
-		setForm((prev) => ({ ...prev, [key]: value }));
-	};
+  const handleFormChange = (key: "email" | "password", value: string) => {
+    setForm((prev) => ({ ...prev, [key]: value }));
+  };
 
-	useEffect(() => {
-		const checkStorage = async () => {
-			try {
-				const storedCredentials = await AsyncStorage.multiGet([
-					storageUserEmailKey,
-					storageUserPasswordKey,
-				]);
+  useEffect(() => {
+    const checkStorage = async () => {
+      try {
+        const storedCredentials = await AsyncStorage.multiGet([
+          storageUserEmailKey,
+          storageUserPasswordKey,
+        ]);
 
-				const email = storedCredentials.find(
-					([key]) => key === storageUserEmailKey
-				)?.[1];
-				const password = storedCredentials.find(
-					([key]) => key === storageUserPasswordKey
-				)?.[1];
-				if (email && password) {
-					setForm({ email, password });
-					await signInWithEmailAndPassword(FirebaseAuth, email, password);
-				}
-			} catch (error) {
-				console.error("Error reading AsyncStorage:", error);
-			}
-		};
+        const email = storedCredentials.find(
+          ([key]) => key === storageUserEmailKey
+        )?.[1];
+        const password = storedCredentials.find(
+          ([key]) => key === storageUserPasswordKey
+        )?.[1];
+        if (email && password) {
+          setForm({ email, password });
+          await signInWithEmailAndPassword(FirebaseAuth, email, password);
+        }
+      } catch (error) {
+        console.error("Error reading AsyncStorage:", error);
+      }
+    };
 
-		checkStorage();
-	}, []);
+    checkStorage();
+  }, []);
 
-	useEffect(() => {
-		const unsubscribe = FirebaseAuth.onAuthStateChanged((user) => {
-			if (user) {
-				router.replace("/home/(tabs)/home");
-			}
-			return () => unsubscribe();
-		});
-	});
-	return (
-		<KeyboardAvoidingView behavior="padding" style={styles.container}>
-			<View style={styles.inputContainer}>
-				<TextInput
-					label="Email"
-					placeholder="Enter email"
-					value={form.email}
-					mode="flat"
-					style={styles.input}
-					onChangeText={(text) => {
-						handleFormChange("email", text);
-						setErrors({});
-					}}
-					autoComplete="email"
-				/>
-				<HelperText
-					style={{ color: "red" }}
-					type="error"
-					visible={!!errors.email}
-				>
-					{errors.email}
-				</HelperText>
-				<TextInput
-					label="Password"
-					mode="flat"
-					placeholder="Enter password"
-					value={form.password}
-					style={styles.input}
-					onChangeText={(text) => {
-						handleFormChange("password", text);
-						setErrors({});
-					}}
-					autoComplete="password"
-					secureTextEntry
-				/>
-				<HelperText
-					style={{ color: "red" }}
-					type="error"
-					visible={!!errors.general || !!errors.password}
-				>
-					{errors.general ?? errors.password}
-				</HelperText>
-			</View>
-			<View style={styles.buttonContainter}>
-				<TouchableOpacity onPress={handleLogin} style={styles.button}>
-					<Text>Login</Text>
-				</TouchableOpacity>
-				<TouchableOpacity
-					onPress={handleSignup}
-					style={[styles.button, styles.buttonOutline]}
-				>
-					<Text style={styles.buttonOutlineText}>Register</Text>
-				</TouchableOpacity>
-			</View>
-		</KeyboardAvoidingView>
-	);
+  useEffect(() => {
+    const unsubscribe = FirebaseAuth.onAuthStateChanged((user) => {
+      if (user) {
+        router.replace("/home/(tabs)/home");
+      }
+      return () => unsubscribe();
+    });
+  });
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          label="Email"
+          placeholder="Enter email"
+          value={form.email}
+          mode="flat"
+          style={styles.input}
+          onChangeText={(text) => {
+            handleFormChange("email", text);
+            setErrors({});
+          }}
+          autoComplete="email"
+        />
+        <HelperText
+          style={{ color: "red" }}
+          type="error"
+          visible={!!errors.email}
+        >
+          {errors.email}
+        </HelperText>
+        <TextInput
+          label="Password"
+          mode="flat"
+          placeholder="Enter password"
+          value={form.password}
+          style={styles.input}
+          onChangeText={(text) => {
+            handleFormChange("password", text);
+            setErrors({});
+          }}
+          autoComplete="password"
+          secureTextEntry
+        />
+        <HelperText
+          style={{ color: "red" }}
+          type="error"
+          visible={!!errors.general || !!errors.password}
+        >
+          {errors.general ?? errors.password}
+        </HelperText>
+      </View>
+      <View style={styles.buttonContainter}>
+        <TouchableOpacity onPress={handleLogin} style={styles.button}>
+          <Text>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleSignup}
+          style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-	},
-	inputContainer: { width: "80%" },
-	input: {
-		marginTop: 5,
-		borderWidth: 1,
-		borderColor: Colors.standartAppColor,
-	},
-	labels: { fontWeight: "bold" },
-	buttonContainter: {
-		width: "60%",
-		justifyContent: "center",
-		alignItems: "center",
-		marginTop: 40,
-	},
-	button: {
-		backgroundColor: Colors.standartAppColor,
-		width: "100%",
-		padding: 15,
-		borderRadius: 10,
-		alignItems: "center",
-	},
-	buttonOutline: {
-		backgroundColor: "white",
-		marginTop: 5,
-		borderColor: Colors.standartAppColor,
-		borderWidth: 2,
-		padding: 10,
-	},
-	buttonText: { color: "white", fontWeight: "700", fontSize: 16 },
-	buttonOutlineText: { color: "blue", fontWeight: "700", fontSize: 16 },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputContainer: { width: "80%" },
+  input: {
+    marginTop: 5,
+    borderWidth: 1,
+    borderColor: Colors.standartAppColor,
+  },
+  labels: { fontWeight: "bold" },
+  buttonContainter: {
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: Colors.standartAppColor,
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: Colors.standartAppColor,
+    borderWidth: 2,
+    padding: 10,
+  },
+  buttonText: { color: "white", fontWeight: "700", fontSize: 16 },
+  buttonOutlineText: { color: "blue", fontWeight: "700", fontSize: 16 },
 });
 
 const parseFirebaseError = (error: any): ErrorType => {
-	const errorType: ErrorType = {};
+  const errorType: ErrorType = {};
 
-	// Handle specific Firebase Auth error codes
-	switch (error.code) {
-		case "auth/email-already-in-use":
-			errorType.email = "Email is already in use.";
-			break;
-		case "auth/invalid-email":
-			errorType.email = "Invalid email address.";
-			break;
-		case "auth/user-not-found":
-			errorType.email = "No user found with this email.";
-			break;
-		case "auth/wrong-password":
-			errorType.password = "Incorrect password.";
-			break;
-		case "auth/weak-password":
-			errorType.password = "Password is too weak.";
-			break;
-		default:
-			errorType.general = "An unknown error occurred. Please try again.";
-	}
+  // Handle specific Firebase Auth error codes
+  switch (error.code) {
+    case "auth/email-already-in-use":
+      errorType.email = "Email is already in use.";
+      break;
+    case "auth/invalid-email":
+      errorType.email = "Invalid email address.";
+      break;
+    case "auth/user-not-found":
+      errorType.email = "No user found with this email.";
+      break;
+    case "auth/wrong-password":
+      errorType.password = "Incorrect password.";
+      break;
+    case "auth/weak-password":
+      errorType.password = "Password is too weak.";
+      break;
+    default:
+      errorType.general = "An unknown error occurred. Please try again.";
+  }
 
-	return errorType;
+  return errorType;
 };
 
 export default LoginScreen;
